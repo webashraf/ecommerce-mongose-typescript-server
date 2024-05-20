dac237ed6abc2f597767b5f29c16d974288da5e9
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-05-20 21:07:37 +0600

    conncet my project to the github

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ["dist/*", ".env", "node_modules"],
+  },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  }
+);
